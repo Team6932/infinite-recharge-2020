6a9d4cbd6e4f55aa87e3e870bfda983f5722de77
Kijan_Games <KijansCreations@gmail.com>
2020-01-28 16:59:52 -0500

    finshed cleaning up code

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -32,11 +32,78 @@ public class Robot extends TimedRobot {
   @Override
   public void robotInit() {
 
-    
-
-
     // camera setup
     CameraServer.getInstance().startAutomaticCapture();
+
+    robotInitDashboard();
+
+    // adds color options to camera sensor code
+    robot.m_colorMatcher.addColorMatch(robot.kBlueTarget);
+    robot.m_colorMatcher.addColorMatch(robot.kGreenTarget);
+    robot.m_colorMatcher.addColorMatch(robot.kRedTarget);
+    robot.m_colorMatcher.addColorMatch(robot.kYellowTarget);
+  
+  }
+
+  @Override
+  public void robotPeriodic() {
+
+    robotPeriodicColor();
+
+    robotPeriodicDashboard();
+
+  }
+
+  @Override
+  public void teleopPeriodic() {
+
+    controllerVars();
+    
+    ballLauncherControl();
+
+    colorWheelControl();
+
+    driveControl();
+
+  }
+
+  @Override
+  public void autonomousInit() {
+
+    robot.m_autoSelected = shuffleboardInit.m_autoChooser.getSelected();// sets variable autoselected to a variable pulled from dashboard
+
+    variables.time = System.currentTimeMillis();// Sets a variable equal to the current time in milliseconds
+    
+    robot.m_imu.reset();// Resets gyro
+    robot.m_imu.calibrate();// Calibrates gyro
+
+  }
+
+  @Override
+  public void autonomousPeriodic() {
+
+    autonomousDrive();
+
+  }
+
+  
+  public double angleZ() {
+
+  return robot.m_imu.getGyroAngleZ();
+  
+}
+  public double gyroAxis() {
+    double axis = robot.m_imu.getAngle();
+
+    if (axis > 360) {
+      axis= axis - 360;
+    }else if (axis < -360) {
+      axis = axis + 360;
+    }
+    return axis * 0.05;
+  }
+
+  public void robotInitDashboard() {
 
     // setup spin import (from shuffleboard)
     shuffleboardInit.spins = shuffleboardInit.tab.add("Spins", 1).withWidget(BuiltInWidgets.kNumberSlider).withProperties(Map.of("min", 3, "max", 5)) // specify
@@ -57,12 +124,6 @@ public class Robot extends TimedRobot {
     // sends the color chooser to shuffleboard
     SmartDashboard.putData("colorChooser", shuffleboardInit.colorChooser);
 
-    // adds color options to camera sensor code
-    robot.m_colorMatcher.addColorMatch(robot.kBlueTarget);
-    robot.m_colorMatcher.addColorMatch(robot.kGreenTarget);
-    robot.m_colorMatcher.addColorMatch(robot.kRedTarget);
-    robot.m_colorMatcher.addColorMatch(robot.kYellowTarget);
-
     // automated stage option chooser
     shuffleboardInit.m_autoChooser.setDefaultOption("Default Auto", variables.kDefaultAuto);
     shuffleboardInit.m_autoChooser.addOption("My Auto", variables.kCustomAuto);
@@ -75,19 +136,23 @@ public class Robot extends TimedRobot {
     SmartDashboard.putBoolean("ConfigCal", false);
     SmartDashboard.putBoolean("Reset", false);
     SmartDashboard.putBoolean("SetYawAxis", false);
-  
   }
-
-  @Override
-  public void robotPeriodic() {
-    System.out.println(variables.testMotor);
-    System.out.println(variables.testMotorSpeed);
-
-    System.out.println("Gyro Angle = " + robot.m_imu.getAngle());
+  public void robotPeriodicDashboard() {
 
     // sends different angles to shuffleboard
     SmartDashboard.putNumber("YawAngle", robot.m_imu.getGyroAngleZ());
 
+    // sends misc. inputs to shuffleboard.
+    shuffleboardInit.dsInfo.getEntry("spinning").setValue(variables.colorSpin);
+    shuffleboardInit.dsInfo.getEntry("searchColor").setValue(variables.searchColor);
+    shuffleboardInit.dsInfo.getEntry("Inverted").setValue(variables.driveInverted);
+    shuffleboardInit.dsInfo.getEntry("Spins Counted").setValue(variables.spinNumber / 8);
+    shuffleboardInit.dsInfo.getEntry("Testing Motor").setValue(variables.testMotor);
+    shuffleboardInit.dsInfo.getEntry("drive straight?!?!??!?!?!?!?!?").setValue(variables.straightDrive);
+
+  }
+  public void robotPeriodicColor() {
+    
     // gets the selected color and sets it to a variable, then checks for the
     // closest color and makes that a variable.
     Color detectedColor = robot.colorSensor.getColor();
@@ -119,35 +184,61 @@ public class Robot extends TimedRobot {
 
     }
 
-    // sends misc. inputs to shuffleboard.
-    shuffleboardInit.dsInfo.getEntry("spinning").setValue(variables.colorSpin);
-    shuffleboardInit.dsInfo.getEntry("searchColor").setValue(variables.searchColor);
-    shuffleboardInit.dsInfo.getEntry("Inverted").setValue(variables.driveInverted);
-    shuffleboardInit.dsInfo.getEntry("Spins Counted").setValue(variables.spinNumber / 8);
-    shuffleboardInit.dsInfo.getEntry("Testing Motor").setValue(variables.testMotor);
-    shuffleboardInit.dsInfo.getEntry("drive straight?!?!??!?!?!?!?!?").setValue(variables.straightDrive);
-
   }
+  public void autonomousDrive() {
+    
+    switch (robot.m_autoSelected) {// checks what option is selected for autonomous
 
-  public double gyroAxis() {
-    double axis = robot.m_imu.getAngle();
-    return axis * 0.05;
+      case Variables.kCustomAuto:
+        break;
+      case Variables.kDefaultAuto:
+      default:
+
+      switch(variables.autoStep) {// DEFAULT: step based system, if a step is "completed" then it moves on to the next one.
+        case 0: {// During step 0 the robot goes straight for 5 seconds
+          if (System.currentTimeMillis() <= variables.time + 5000){// compares current time to starting time + 5 seconds
+            System.out.println("Turn angle = " + gyroAxis());// prints gyro angle for testing purpouses
+            robot.drive.arcadeDrive(0.5, -gyroAxis());// moves forward, correcting itself 
+          }else {// Once complete increases to the next step
+            variables.autoStep++;
+          }
+          break;
+        }
+        case 1: {// During step 1 the robot turns around 50 degrees
+          resetGyro();//sets curent angle to 0 degrees
+          if(angleZ() <= 50) {// while the robot is less than 50 degrees
+            robot.drive.arcadeDrive(0, 0.5); // Turn    
+          } else {
+            variables.autoStep++;// increases the step once this step is completed
+          }
+          break;
+        }        
+        case 2: {// During step 2 the robot turns back 50 degrees, leaving it where it started
+            if (angleZ() > 0) {// while the degree is greater than 0
+              robot.drive.arcadeDrive(0, -0.5);// turns the robot back
+            }
+            else {
+              variables.autoStep++; // increases the step
+            }
+            break;
+        }
+      }
+        break;
+    }
   }
-
-  // TODO Finish adding code
-
-  @Override
-  public void teleopPeriodic() {
-
-    // gets the color from the color sensor, and then sets a variable for the
-    // detected color.
-    Color detectedColor = robot.colorSensor.getColor();
-    ColorMatchResult match = robot.m_colorMatcher.matchClosestColor(detectedColor);
+  public void resetGyro() {
+  if (!variables.gyroResetP) {
+    robot.m_imu.reset();
+    robot.m_imu.calibrate();
+    variables.gyroResetP = true;
+  }
+}
+  public void controllerVars() {
 
     // detects button inputs and inverts a variable (basically makes the button act
     // like switches instead of buttons)
     if (robot.controller.getRawButtonPressed(1))
-    variables.searchColor = !variables.searchColor;
+      variables.searchColor = !variables.searchColor;
     if (robot.controller.getRawButtonPressed(10))
     variables.colorSpin = !variables.colorSpin;
     if (robot.joystick.getRawButtonPressed(2))
@@ -167,11 +258,9 @@ public class Robot extends TimedRobot {
       variables.straightDrive = !variables.straightDrive;
     }
 
-    // debugging (prints out special variables of my choosing)
-    /*System.out.println("Spin number = " + spinNumber);
-    System.out.println("previous color = " + kPreviousColor);
-    System.out.println("current color = " + match.color);*/
-
+  }
+  public void ballLauncherControl() {
+    
     // controls the ball launcher
     if (variables.ballLauncher) {// ball launcher control
 
@@ -187,46 +276,59 @@ public class Robot extends TimedRobot {
 
     }
 
+  }
+  public void colorWheelControl() {
+
+    // gets the color from the color sensor, and then sets a variable for the
+    // detected color.
+    Color detectedColor = robot.colorSensor.getColor();
+    ColorMatchResult match = robot.m_colorMatcher.matchClosestColor(detectedColor);
+
     // controls the color wheel motors and color sensor
     if (variables.searchColor && !variables.colorSpin) {// if the color specific wheel command is being run, and not the spin ammount
-                                    // command
-      if (variables.kSelectedTarget != match.color) {// if the color seen is not equal to the color chosen by the user
-        robot.spinnerMotor.set(0.3);// spin the motor
-      } else {// if the colors are the same
-        robot.spinnerMotor.stopMotor();// stop the motor
-        variables.colorSpin = false;// disable command
-        // TODO spinn motor X amount to put sensor in right spot.
-      }
-    } else if (variables.colorSpin && !variables.searchColor) {// if the spin ammount is run and not the color searcher
-      if (variables.spinNumber / 8 < shuffleboardInit.spins.getDouble(3.0)) {// spin the wheel untill the number of complete spins is equal to the
-                                                  // user specified number
+      // command
+if (variables.kSelectedTarget != match.color) {// if the color seen is not equal to the color chosen by the user
+robot.spinnerMotor.set(0.3);// spin the motor
+} else {// if the colors are the same
+robot.spinnerMotor.stopMotor();// stop the motor
+variables.colorSpin = false;// disable command
+// TODO spinn motor X amount to put sensor in right spot.
+}
+} else if (variables.colorSpin && !variables.searchColor) {// if the spin ammount is run and not the color searcher
+if (variables.spinNumber / 8 < shuffleboardInit.spins.getDouble(3.0)) {// spin the wheel untill the number of complete spins is equal to the
+                    // user specified number
 
-        if (variables.spinNumber == 0) {
-          variables.kPreviousColor = match.color;
-        }
+if (variables.spinNumber == 0) {
+variables.kPreviousColor = match.color;
+}
 
-        robot.spinnerMotor.set(0.3);
+robot.spinnerMotor.set(0.3);
 
-        if (variables.kPreviousColor != match.color) {// adds 1 to the spin number every time it sees a new color (this is divided
-                                                      // by 8)
-          variables.spinNumber++;
-          variables.kPreviousColor = match.color;// sets the current color to the color to the previous color
-        }
-      } else {// stops the motor if the number of full spins is reached
-        robot.spinnerMotor.stopMotor();
-        variables.colorSpin = false;// stops the command
-        variables.spinNumber = 0;// resets the variable for future use
-      }
-    } else if (variables.colorSpin && variables.searchColor) {// if both are enabled, then both will be automatically disabled to stop
-                                          // conflicts
-      variables.colorSpin = false;// disables number specific spin
-      variables.searchColor = false;// disables color specific spin
+if (variables.kPreviousColor != match.color) {// adds 1 to the spin number every time it sees a new color (this is divided
+                        // by 8)
+variables.spinNumber++;
+variables.kPreviousColor = match.color;// sets the current color to the color to the previous color
+}
+} else {// stops the motor if the number of full spins is reached
+robot.spinnerMotor.stopMotor();
+variables.colorSpin = false;// stops the command
+variables.spinNumber = 0;// resets the variable for future use
+}
+} else if (variables.colorSpin && variables.searchColor) {// if both are enabled, then both will be automatically disabled to stop
+            // conflicts
+variables.colorSpin = false;// disables number specific spin
+variables.searchColor = false;// disables color specific spin
 
-    } else {// stops motors and sets the previous color to null (for the number counter) so
-            // it starts fresh
-      robot.spinnerMotor.stopMotor();// stops motor
-      variables.kPreviousColor = null;// resets previous color variable
-    }
+} else {// stops motors and sets the previous color to null (for the number counter) so
+// it starts fresh
+robot.spinnerMotor.stopMotor();// stops motor
+variables.kPreviousColor = null;// resets previous color variable
+}
+
+
+  }
+  public void motorTest() {
+
 
     // Test the motor robot.controller and motors
     if (variables.testMotor && variables.testMotorSpeed <= 1) {
@@ -261,6 +363,9 @@ public class Robot extends TimedRobot {
 
     }
 
+  }
+  public void driveControl() {
+    
     // Drive with arcade drive.
     // That means that the Y axis drives forward.
     // and backward, and the X turns left and right.
@@ -273,90 +378,4 @@ public class Robot extends TimedRobot {
 
   }
 
-  @Override
-
-  public void autonomousInit() {
-
-    robot.m_autoSelected = shuffleboardInit.m_autoChooser.getSelected();// sets variable autoselected to a variable pulled from dashboard
-                                                 // earlier
-
-    // m_autoSelected = SmartDashboard.getString("Auto Selector", kDefaultAuto);
-
-    System.out.println("Auto selected: " + robot.m_autoSelected);// prints the variable
-
-    variables.time = System.currentTimeMillis();
-
-    
-    robot.m_imu.reset();
-    //robot.m_imu.configCalTime(1);
-    robot.m_imu.calibrate();
-    //robot.m_imu.reset();
-
-  }
-
-public double angleZ() {
-
-  return robot.m_imu.getGyroAngleZ();
 }
-
-public void resetGyro() {
-  if (!variables.gyroResetP) {
-    robot.m_imu.reset();
-    robot.m_imu.calibrate();
-    variables.gyroResetP = true;
-  }
-}
-
-  @Override
-
-  public void autonomousPeriodic() {
-
-    switch (robot.m_autoSelected) {
-
-      case Variables.kCustomAuto:
-
-        break;
-
-      case Variables.kDefaultAuto:
-
-      default:
-
-      switch(variables.autoStep) {
-        case 0: {
-          if (System.currentTimeMillis() <= variables.time + 5000){
-            System.out.println("Turn angle = " + gyroAxis());
-            robot.drive.arcadeDrive(0.5, -gyroAxis());
-          }else {
-            variables.autoStep++;
-          }
-          break;
-        }
-        case 1: {
-          resetGyro();
-          if(angleZ() <= 50) {
-            robot.drive.arcadeDrive(0, 0.5);          
-          } else {
-            variables.autoStep++;
-          }
-          break;
-        }        
-        case 2: {
-            if (angleZ() >= 0) {
-              robot.drive.arcadeDrive(0, -0.5);
-            }
-            else {
-              variables.autoStep++;
-            }
-            break;
-        }
-      }
-
-        break;
-
-    }
-
-  }
-  
-
-}
-
